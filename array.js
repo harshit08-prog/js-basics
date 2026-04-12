4cfd687fb015262d5aa89a1c187ea5c1eec322b4
harshit08-prog <h1667651@gmail.com>
2026-04-12 13:47:26 +0000

    array.1

---
+++ array.js
@@ -0,0 +1,27 @@
+
+
+const arr =[1,2,3,4,5,6]
+const heros =[ "ironman", "spidey"];
+console.log(arr[2]);
+console.log(heros[1]);
+const arr1 = new Array(1,2,3,4,5);
+//methodsss++++++++++++++++++++++++++++++++++++++++++++++++
+arr.push(7);
+console.log(arr);
+arr.pop()
+console.log(arr);
+arr.unshift(9);
+console.log(arr);
+arr.shift();
+console.log(arr);
+console.log(arr.indexOf(4));
+console.log(arr.includes(5));
+const newArr = arr.join();
+console.log(newArr);////string bngyaaa join function use krke
+const new1 = [1,2,3,4,5]
+console.log(new1);
+const new2 = new1.slice(1,3)
+console.log(new2);
+const new3 = new1.splice(1,3)
+console.log(new1)
+console.log(new3)
